4877496cd339c62eaf1674fc6d22a0fb75036e5f
barryjung <wjdgusrbs5@gmail.com>
2023-03-24 19:25:15 +0900

    start project, received form

---
+++ app.py
@@ -0,0 +1,20 @@
+class Character:
+    """
+    모든 캐릭터의 모체가 되는 클래스
+    """
+
+    def __init__(self, name, hp, power):
+        self.name = name
+        self.max_hp = hp
+        self.hp = hp
+        self.power = power
+
+    def attack(self, other):
+        damage = random.randint(self.power - 2, self.power + 2)
+        other.hp = max(other.hp - damage, 0)
+        print(f"{self.name}의 공격! {other.name}에게 {damage}의 데미지를 입혔습니다.")
+        if other.hp == 0:
+            print(f"{other.name}이(가) 쓰러졌습니다.")
+
+    def show_status(self):
+        print(f"{self.name}의 상태: HP {self.hp}/{self.max_hp}")
